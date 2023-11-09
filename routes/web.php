a954694e60c8a8df114d1875f65ae6b92ed8cfb9
vinifred1003 <viniciusfrederico1003@gmail.com>
2023-11-08 21:05:01 -0300

    mais métodos

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/empresa', function(){
+    return view('site/empresa');
+});
+
+Route::any('/any', function(){
+    return "Permite todo tipo de acesso {get, put, delete, post}";
+});
+
+Route::match(['put','delete'], '/match', function(){
+    return "Permite apenas acessos definidos";
+});
+
+Route:: get('/produto/{id}/{cat?}', function($id,$cat = ''){
+    return "O id do produto é:".$id."<br>"."A categoria é:".$cat;
+});
