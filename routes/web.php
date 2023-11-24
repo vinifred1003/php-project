2bbd879a5ccd6d8ccdb5f2b879787027e1552640
vinifred1003 <viniciusfrederico1003@gmail.com>
2023-11-24 15:49:31 -0300

    .

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\ProdutoController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,22 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route:: resource('produtos', ProdutoController::class);
 
-Route::get('/empresa', function(){
-    return view('site/empresa');
-});
-
-Route::any('/any', function(){
-    return "Permite todo tipo de acesso {get, put, delete, post}";
-});
-
-Route::match(['put','delete'], '/match', function(){
-    return "Permite apenas acessos definidos";
-});
-
-Route:: get('/produto/{id}/{cat?}', function($id,$cat = ''){
-    return "O id do produto é:".$id."<br>"."A categoria é:".$cat;
-});
